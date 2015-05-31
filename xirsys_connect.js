db6d434cdd4d2901994eda2bc1a5222d92169112
Pachpict <monty@umpire.com>
2015-05-31 17:04:42 +0100

    Simplifying different between secure and insecure 'ident' and 'secret' usage by signal demo

--- xirsys_connect.js
+++ xirsys_connect.js
@@ -1,12 +1,26 @@
+// Ident and secret should ideally be passed from a server for security purposes.
+// If serverAuthentication is true then you should remove these two values.
+
+// Insecure method
 var xirsysConnect = {
 	secure : false,
-	// Server only needed if secure is true.
-	server : '/getToken.php',
-	domain : 'www.xirsys.com',
-	application : 'default',
-	room : 'default',
-	// Ident and secret should ideally be passed from a server for security purposes.
-	// If serverAuthentication is true then you can discard these two values.
-	ident : 'user-1',
-	secret : '12345678-1234-1234-1234-123456789012'
+	data : {
+		domain : 'www.xirsys.com',
+		application : 'default',
+		room : 'default',
+		ident : 'user-1',
+		secret : '12345678-1234-1234-1234-123456789012'
+	}
 };
+
+// Secure method
+/*var xirsysConnect = {
+	secure : true,
+	server : '/getToken.php',
+	info : {
+		domain : 'www.xirsys.com',
+		application : 'default',
+		room : 'default'
+	}
+};*/
+
