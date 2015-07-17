d9b52f4044ceb0ca5ed4f4bf3d25760ae7fab9bb
Lee Sylvester <lee.sylvester@gmail.com>
2015-07-17 21:24:08 +0100

    Updated with improved SimpleWebRTC example

--- xirsys_connect.js
+++ xirsys_connect.js
@@ -1,25 +1,28 @@
 // Ident and secret should ideally be passed from a server for security purposes.
 // If serverAuthentication is true then you should remove these two values.
 
-var xirsysConnect = {
-	secure : false,
-	data : {
-		domain : 'www.your-domain.com',
-		application : 'default',
-		room : 'default',
-		ident : 'your-ident',
-		secret : 'your-secret'
-	}
-};
+// var xirsysConnect = {
+// 	secure : false,
+// 	data : {
+// 		domain : 'www.your-domain.com',
+// 		application : 'default',
+// 		room : 'default',
+// 		ident : 'your-ident',
+// 		secret : 'your-secret'
+// 	}
+// };
 
 // Secure method
-/*var xirsysConnect = {
+var xirsysConnect = {
 	secure : true,
-	server : '/getToken.php',
-	info : {
-		domain : 'www.your-domain.com',
+	token_url : 'https://service.xirsys.com/signal/token',
+	ice_url : 'https://service.xirsys.com/ice',
+	data : {
+		domain : 'www.xirsys.com',
 		application : 'default',
-		room : 'default'
+		room : 'default',
+		ident : '***REMOVED***',
+		secret : '***REMOVED***'
 	}
-};*/
+};
 
