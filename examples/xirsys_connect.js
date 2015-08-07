8c8403c3da90c9dffcfba230bc04706ef0e1c75d
Lazarus404 <lee.sylvester@gmail.com>
2015-08-07 19:06:55 +0100

    Rearranged examples html files

---
+++ examples/xirsys_connect.js
@@ -0,0 +1,27 @@
+// Ident and secret should ideally be passed from a server for security purposes.
+// If serverAuthentication is true then you should remove these two values.
+
+// var xirsysConnect = {
+// 	secure : false,
+// 	data : {
+// 		domain : 'www.your-domain.com',
+// 		application : 'default',
+// 		room : 'default',
+// 		ident : 'your-ident',
+// 		secret : 'your-secret'
+// 	}
+// };
+
+// Secure method
+var xirsysConnect = {
+	secure : true,
+	token_url : 'https://service.xirsys.com/signal/token',
+	ice_url : 'https://service.xirsys.com/ice',
+	room_url : 'https://service.xirsys.com/room',
+	data : {
+		domain : 'www.xirsys.com',
+		application : 'default',
+		room : 'default'
+	}
+};
+
